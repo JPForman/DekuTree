11f99cec9ea160cf67021ba36ff66a46cf9edf37
Joshua Forman <cyborgforman@gmail.com>
2020-01-17 12:55:09 -0800

    update create method of reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,20 @@ class ReviewsController < ApplicationController
     @review = @product.reviews.new
     render :new
   end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def song_params
+    params.require(:song).permit(:name, :lyrics)
+  end
 end
