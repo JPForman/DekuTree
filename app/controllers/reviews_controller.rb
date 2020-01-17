1960eb7a5fbd79eed33c89dfae5c456c4449e53b
Joshua Forman <cyborgforman@gmail.com>
2020-01-17 13:15:05 -0800

    add reviews erbs and introduce some bugs

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,6 +21,21 @@ class ReviewsController < ApplicationController
     render :show
   end
 
+  def update
+  @review = Review.find(params[:id])
+  if @review.update(review_params)
+    redirect_to product_path(@review.product)
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @review = Review.find(params[:id])
+  @review.destroy
+  redirect_to product_path(@review.product)
+end
+
   private
   def review_params
     params.require(:review).permit(:title, :author, :rating)
