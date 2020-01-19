4fae21fedae817c080325c66d357fd3c3f2fc03b
Joshua Forman <cyborgforman@gmail.com>
2020-01-19 14:08:05 -0800

    add Faker gem and seeding for products and reviews

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'jquery-rails'
 
+gem 'faker'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Dessert.variety, cost: (rand 1000), country_of_origin: Faker::Address.country)
+
+
+end
+p "Created #{Product.count} products"
+
+
+
+Review.destroy_all
+
+250.times do |index|
+  Review.create!(title: Faker::Coffee.notes, author: Faker::Science.scientist , content_body: Faker::Lorem.characters(number: (rand 50...250)), rating: (rand 0...5), product_id: Product.all.ids.sample)
+end
+p "Created #{Review.count} reviews"
