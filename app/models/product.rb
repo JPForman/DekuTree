fc4d4d0f052044bfec52d17f440774339672b17c
Joshua Forman <cyborgforman@gmail.com>
2020-01-20 11:21:40 -0800

    add scope for 3 most recent products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
 end
