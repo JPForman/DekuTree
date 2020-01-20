8595ba0d27843f227ac2b172a19c787d72cf9ff6
Joshua Forman <cyborgforman@gmail.com>
2020-01-20 11:52:00 -0800

    add scope and display of products made in the USA

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ApplicationRecord
   validates :cost, presence: true
   validates :country_of_origin, presence: true
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :usa, -> { where(country_of_origin: "USA") }
 end
