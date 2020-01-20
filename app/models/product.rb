2d37f63d30964fcb0d5c2635e1e72100ff75f840
Joshua Forman <cyborgforman@gmail.com>
2020-01-20 12:10:26 -0800

    add scope and front end for most reviewed product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,12 @@ class Product < ApplicationRecord
   validates :country_of_origin, presence: true
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
   scope :usa, -> { where(country_of_origin: "USA") }
+
+  scope :most_reviewed, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+   .joins(:reviews)
+   .group("products.id")
+   .order("reviews_count DESC")
+   .limit(1)
+  )}
 end
