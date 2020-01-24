eecb398729006588ed42c26f0b3102e6bf0543e9
Joshua Forman <cyborgforman@gmail.com>
2020-01-24 13:26:37 -0800

    fine tuned permissions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize_admin, only: [:destroy]
+  before_action :authorize_admin, only: [:destroy, :create, :edit, :update]
 
   def index
     @products = Product.all
